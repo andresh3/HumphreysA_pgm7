e4fd9ce608780e363286e69402fe3f62a05d36a3
Andrew S Humphreys <andresh@bgsu.edu>
2023-04-19 13:21:53 -0400

    Added logic in HumphreysA_pgm7.cpp

--- HumphreysA_pgm7.cpp
+++ HumphreysA_pgm7.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 const int MAX_PLAYERS = 50;
 const string FILE_NAME = "centers_rebs_20_21-1.txt";
 
-bool read_data(ifstream& fs, string player_names[], double rebound_averages[], int players)
+bool read_data(ifstream& fs, string player_names[], double rebound_averages[], int& players)
 {
 
 	fs.open(FILE_NAME);
@@ -14,7 +15,14 @@ bool read_data(ifstream& fs, string player_names[], double rebound_averages[], i
 	if (!fs.is_open() || fs.bad())
 		return false;
 
-	cout << "FIX ME: read_data" << endl;
+	int i = 0;
+	while (!fs.eof() && fs.good())
+	{
+		fs >> player_names[i];
+		fs >> rebound_averages[i];
+		i++;
+	}
+	players = i; 
 
 
 	fs.close();
@@ -22,17 +30,43 @@ bool read_data(ifstream& fs, string player_names[], double rebound_averages[], i
 
 void print_data( const string keys[], const double values[], const int arr_len)
 {
-	cout << "FIX ME: print_data(string keys[], double values[], int arr_len)" << endl;
+	for (int i = 0; i < arr_len; i++)
+	{
+		cout << left << setw(25) << keys[i] << fixed << setprecision(1) << setw(5) << right << values[i] << endl;
+	}
 	return;
 }
 void print_data(const string keys[], const int arr_len)
 {
-	cout << "FIX ME: print_data(string keys[], int arr_len)" << endl;
+	for (int i = 0; i < arr_len; i++)
+	{
+		cout << left << setw(25) << keys[i] << endl;
+	}
 	return;
 }
 
 void alphabetize_arr(string sort_arr[], double parrallel_arr[], int arr_size, bool ascending=true)
 {
+	// "ascending modifier"
+	int asc_mod = 1;
+	if (!ascending)
+		asc_mod = -1;
+
+
+	// SELECTION SORT
+	for (int i = 0; i < arr_size; i++)
+	{
+		int smallest_val_index = i;
+		int smallest_val = sort_arr[i];
+		for (int j = i; j++; j < arr_size)
+		{
+			int curr_val = sort_arr[j];
+			if (curr_val * asc_mod < smallest_val)
+			{
+
+			}
+		}
+	}
 	cout << "FIX ME: alphabetize_arr(string sort_arr[], double parralel_arr, int arr_size)" << endl;
 }
 
@@ -47,7 +81,7 @@ int main()
 	ifstream in_fs;
 	string player_names[MAX_PLAYERS];
 	double rebound_averages[MAX_PLAYERS];
-	int players = MAX_PLAYERS;
+	int players = 0;
 
 	if (!read_data(in_fs, player_names, rebound_averages, players))
 	{
